fbb329e05407431288134215ba9f919675827e4a
Haseeb ul Rehman <haseeb.rehman@ideofuzion.com>
2024-10-08 10:46:57 +0500

    rename eslint and ts config packages

--- packages/ui-lib/.eslintrc.js
+++ packages/ui-lib/.eslintrc.js
@@ -1,5 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  extends: ["@repo/config-eslint/react.js"],
   parser: "@typescript-eslint/parser",
 };
